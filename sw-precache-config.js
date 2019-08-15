76132c9a2d66fc6cb895191f28d5b47f654920a6
Alexander Kunin <alexander.kunin@gmail.com>
2019-08-15 10:43:24 -0700

    offline

---
+++ sw-precache-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: 'docs',
+  root: 'docs/',
+  staticFileGlobs: [
+    'docs/index.html',
+    'docs/**.js',
+    'docs/**.css',
+    'docs/assets/data/**.json'
+  ]
+};
